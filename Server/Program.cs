769aaa6f852e76edf0d29607ed8c2abe9889b41b
Đorđe Milanović <djordje200179@gmail.com>
2023-07-31 16:01:47 +0200

    Server responses improved

--- Server/Program.cs
+++ Server/Program.cs
@@ -2,6 +2,7 @@ using LindaSharp;
 using LindaSharp.Server;
 using JSON = Microsoft.AspNetCore.Http.Json;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,25 +18,53 @@ var linda = new LocalLinda();
 app.MapGet("/ping", () => "pong");
 
 app.MapPost("/out", ([FromBody] object[] tuple) => {
-	linda.Out(tuple);
+	try {
+		linda.Out(tuple);
+	} catch (ObjectDisposedException) {
+		return Results.StatusCode((int)HttpStatusCode.InternalServerError);
+	}
 
-	return "OK";
+	return Results.StatusCode((int)HttpStatusCode.Created);
 });
 
 app.MapGet("/in", ([FromBody] object?[] tuplePattern) => {
-	return linda.In(tuplePattern);
+	try {
+		var tuple = linda.In(tuplePattern);
+
+		return Results.Json(tuple);
+	} catch (ObjectDisposedException) {
+		return Results.StatusCode((int)HttpStatusCode.InternalServerError);
+	}
 });
 
 app.MapGet("/rd", ([FromBody] object?[] tuplePattern) => {
-	return linda.Rd(tuplePattern);
+	try {
+		var tuple = linda.Rd(tuplePattern);
+
+		return Results.Json(tuple);
+	} catch (ObjectDisposedException) {
+		return Results.StatusCode((int)HttpStatusCode.InternalServerError);
+	}
 });
 
 app.MapGet("/inp", ([FromBody] object?[] tuplePattern) => {
-	return linda.Inp(tuplePattern, out var tuple) ? tuple : null;
+	try {
+		var status = linda.Inp(tuplePattern, out var tuple);
+
+		return status ? Results.Json(tuple) : Results.NotFound();
+	} catch (ObjectDisposedException) {
+		return Results.StatusCode((int)HttpStatusCode.InternalServerError);
+	}
 });
 
 app.MapGet("/rdp", ([FromBody] object?[] tuplePattern) => {
-	return linda.Rdp(tuplePattern, out var tuple) ? tuple : null;
+	try {
+		var status = linda.Rdp(tuplePattern, out var tuple);
+
+		return status ? Results.Json(tuple) : Results.NotFound();
+	} catch (ObjectDisposedException) {
+		return Results.StatusCode((int)HttpStatusCode.InternalServerError);
+	}
 });
 
 app.Run();
