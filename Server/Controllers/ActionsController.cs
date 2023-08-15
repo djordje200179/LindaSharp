cccbfa3ceebabd12f5df9db61215b70a07998bd0
Đorđe Milanović <djordje200179@gmail.com>
2023-08-15 21:46:30 +0200

    Server converted from minimal API to controller based

---
+++ Server/Controllers/ActionsController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace LindaSharp.Server.Controllers;
+
+[Route("/")]
+[ApiController]
+public class ActionsController : ControllerBase {
+	private readonly SharedLinda linda;
+
+	public ActionsController(SharedLinda linda) {
+		this.linda = linda;
+	}
+
+	[HttpPost("/out")]
+	public IActionResult Out([FromBody] object[] tuple) {
+		try {
+			linda.Out(tuple);
+		} catch (ObjectDisposedException) {
+			return StatusCode((int)HttpStatusCode.InternalServerError);
+		}
+
+		return Created("/rd", tuple);
+	}
+
+	[HttpDelete("/in")]
+	public IActionResult In([FromBody] object?[] tuplePattern) {
+		try {
+			var tuple = linda.In(tuplePattern);
+
+			return Ok(tuple);
+		} catch (ObjectDisposedException) {
+			return StatusCode((int)HttpStatusCode.InternalServerError);
+		}
+	}
+
+	[HttpGet("/rd")]
+	public IActionResult Rd([FromBody] object?[] tuplePattern) {
+		try {
+			var tuple = linda.Rd(tuplePattern);
+
+			return Ok(tuple);
+		} catch (ObjectDisposedException) {
+			return StatusCode((int)HttpStatusCode.InternalServerError);
+		}
+	}
+
+	[HttpDelete("/inp")]
+	public IActionResult Inp([FromBody] object?[] tuplePattern) {
+		try {
+			var status = linda.Inp(tuplePattern, out var tuple);
+
+			return status ? Ok(tuple) : NotFound();
+		} catch (ObjectDisposedException) {
+			return StatusCode((int)HttpStatusCode.InternalServerError);
+		}
+	}
+
+	[HttpGet("/rdp")]
+	public IActionResult Rdp([FromBody] object?[] tuplePattern) {
+		try {
+			var status = linda.Rdp(tuplePattern, out var tuple);
+
+			return status ? Ok(tuple) : NotFound();
+		} catch (ObjectDisposedException) {
+			return StatusCode((int)HttpStatusCode.InternalServerError);
+		}
+	}
+
+	[HttpPost("/eval")]
+	public IActionResult Eval() {
+		var request = HttpContext.Request;
+
+		StreamReader reader;
+		if (request.ContentType == "text/ironpython")
+			reader = new StreamReader(request.Body);
+		else if (request.Form.Files.Count == 1) {
+			var file = request.Form.Files[0];
+			reader = new StreamReader(file.OpenReadStream());
+		} else
+			return BadRequest();
+
+		var codeReadingTask = reader.ReadToEndAsync();
+		codeReadingTask.Wait();
+		var ironpythonCode = codeReadingTask.Result;
+
+		linda.Eval(ironpythonCode);
+
+		return Ok();
+	}
+
+	[HttpPut("/eval/{key}")]
+	public IActionResult EvalRegister(string key) {
+		var request = HttpContext.Request;
+
+		StreamReader reader;
+		if (request.ContentType == "text/ironpython")
+			reader = new StreamReader(request.Body);
+		else if (request.Form.Files.Count == 1) {
+			var file = request.Form.Files[0];
+			reader = new StreamReader(file.OpenReadStream());
+		} else
+			return BadRequest();
+
+		var codeReadingTask = reader.ReadToEndAsync();
+		codeReadingTask.Wait();
+		var ironpythonCode = codeReadingTask.Result;
+
+		linda.EvalRegister(key, ironpythonCode);
+
+		return Ok();
+	}
+
+	[HttpPost("/eval/{key}")]
+	public IActionResult EvalInvoke(string key, [FromBody] object? parameter) {
+		try {
+			linda.EvalInvoke(key, parameter);
+		} catch (KeyNotFoundException) {
+			return NotFound();
+		}
+
+		return Ok();
+	}
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,119 +1,18 @@
 using LindaSharp;
 using LindaSharp.Server;
-using JSON = Microsoft.AspNetCore.Http.Json;
-using Microsoft.AspNetCore.Mvc;
-using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.Configure<JSON.JsonOptions>(options => {
-	options.SerializerOptions.Converters.Add(new TupleJsonDeserializer());
-	options.SerializerOptions.WriteIndented = true;
-});
+builder.Services
+	.AddSingleton(new SharedLinda(new LocalLinda()))
+	.AddControllers()
+	.AddJsonOptions(options => {
+		options.JsonSerializerOptions.Converters.Add(new TupleJsonDeserializer());
+		options.JsonSerializerOptions.WriteIndented = true;
+	});
 
 var app = builder.Build();
 
-var localLinda = new LocalLinda();
-var linda = new SharedLinda(localLinda);
-
-app.MapGet("/ping", () => "pong");
-
-app.MapPost("/out", ([FromBody] object[] tuple) => {
-	try {
-		linda.Out(tuple);
-	} catch (ObjectDisposedException) {
-		return Results.StatusCode((int)HttpStatusCode.InternalServerError);
-	}
-
-	return Results.StatusCode((int)HttpStatusCode.Created);
-});
-
-app.MapDelete("/in", ([FromBody] object?[] tuplePattern) => {
-	try {
-		var tuple = linda.In(tuplePattern);
-
-		return Results.Json(tuple);
-	} catch (ObjectDisposedException) {
-		return Results.StatusCode((int)HttpStatusCode.InternalServerError);
-	}
-});
-
-app.MapGet("/rd", ([FromBody] object?[] tuplePattern) => {
-	try {
-		var tuple = linda.Rd(tuplePattern);
-
-		return Results.Json(tuple);
-	} catch (ObjectDisposedException) {
-		return Results.StatusCode((int)HttpStatusCode.InternalServerError);
-	}
-});
-
-app.MapDelete("/inp", ([FromBody] object?[] tuplePattern) => {
-	try {
-		var status = linda.Inp(tuplePattern, out var tuple);
-
-		return status ? Results.Json(tuple) : Results.NotFound();
-	} catch (ObjectDisposedException) {
-		return Results.StatusCode((int)HttpStatusCode.InternalServerError);
-	}
-});
-
-app.MapGet("/rdp", ([FromBody] object?[] tuplePattern) => {
-	try {
-		var status = linda.Rdp(tuplePattern, out var tuple);
-
-		return status ? Results.Json(tuple) : Results.NotFound();
-	} catch (ObjectDisposedException) {
-		return Results.StatusCode((int)HttpStatusCode.InternalServerError);
-	}
-});
-
-app.MapPost("/eval", (HttpRequest request) => {
-	StreamReader reader;
-	if (request.ContentType == "text/ironpython")
-		reader = new StreamReader(request.Body);
-	else if (request.Form.Files.Count == 1) {
-		var file = request.Form.Files[0];
-		reader = new StreamReader(file.OpenReadStream());
-	} else
-		return Results.StatusCode((int)HttpStatusCode.UnsupportedMediaType);
-
-	var codeReadingTask = reader.ReadToEndAsync();
-	codeReadingTask.Wait();
-	var ironpythonCode = codeReadingTask.Result;
-
-	linda.Eval(ironpythonCode);
-
-	return Results.Ok();
-});
-
-app.MapPut("/eval/{key}", (HttpRequest request, string key) => {
-	StreamReader reader;
-	if (request.ContentType == "text/ironpython")
-		reader = new StreamReader(request.Body);
-	else if (request.Form.Files.Count == 1) {
-		var file = request.Form.Files[0];
-		reader = new StreamReader(file.OpenReadStream());
-	} else
-		return Results.StatusCode((int)HttpStatusCode.UnsupportedMediaType);
-
-	var codeReadingTask = reader.ReadToEndAsync();
-	codeReadingTask.Wait();
-	var ironpythonCode = codeReadingTask.Result;
-
-	linda.EvalRegister(key, ironpythonCode);
-
-	return Results.Ok();
-});
-
-app.MapPost("/eval/{key}", (HttpRequest request, string key, [FromBody] object? parameter) => {
-	try {
-		linda.EvalInvoke(key, parameter);
-	} catch (KeyNotFoundException) {
-		return Results.NotFound();
-	}
-
-	return Results.Ok();
-});
+app.MapControllers();
 
 app.Run();
