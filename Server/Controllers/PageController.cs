d596b1356b62a7e576e3b951a8fdf76c89745500
Đorđe Milanović <djordje200179@gmail.com>
2023-08-19 20:19:08 +0200

    Linda tuples visualisation realised

--- Server/Controllers/PageController.cs
+++ Server/Controllers/PageController.cs
@@ -3,8 +3,15 @@
 namespace LindaSharp.Server.Controllers;
 
 public class PageController : Controller {
+	private readonly SharedLinda linda;
+
+	public PageController(SharedLinda linda) {
+		this.linda = linda;
+	}
+
 	[Route("/")]
 	public IActionResult Index() {
+		ViewBag.TupleSpace = linda.ReadAll();
 		return View();
 	}
 }
