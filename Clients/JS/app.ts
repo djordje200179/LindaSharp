580be20e228698309775714d2e319ad5264fed3f
Đorđe Milanović <djordje200179@gmail.com>
2023-08-22 17:35:58 +0200

    JS client finished

--- Clients/JS/app.ts
+++ Clients/JS/app.ts
@@ -1,55 +1,97 @@
+class LindaDisposedError extends Error {
+	public constructor() {
+		super("Server Linda was disposed");
+
+		this.name = this.constructor.name;
+	}
+}
+
 class RemoteLinda {
 	private baseUrl: string;
+	private actionsUrl: string;
+	private healthUrl: string;
 
 	public constructor(host: string, port: number) {
-		this.baseUrl = `http://${host}:${port}/actions/`
+		this.baseUrl = `http://${host}:${port}/`;
+		this.actionsUrl = this.baseUrl + "actions/";
+		this.healthUrl = this.baseUrl + "health/";
 	}
 
-	private sendTextRequest(method: string, path: string, data: any, contentType: string) {
-		
+	private async sendTextRequest(method: string, path: string, data: any, contentType: string): Promise<Response> {
+		const url = this.actionsUrl + path;
+		const headers = {
+			"Content-type": contentType
+		};
+		const response = await fetch(url, {
+			method: method,
+			headers: headers,
+			body: data
+		});
+
+		if (response.status == 500)
+			throw new LindaDisposedError();
+
+		return response;
 	}
 
-	private sendJsonRequest(method: string, path: string, data: any) {
-
+	private sendJsonRequest(method: string, path: string, data: any): Promise<Response> {
+		return this.sendTextRequest(method, path, JSON.stringify(data), "application/json");
 	}
 
-	private waitTuple(tuplePattern: any[], del: boolean): any[] {
-
+	private async waitTuple(tuplePattern: any[], del: boolean): Promise<any[]> {
+		const method = del ? "DELETE" : "GET";
+		const path = del ? "rd" : "in";
+		const response = await this.sendJsonRequest(method, path, tuplePattern);
+		return await response.json();
 	}
 
-	private tryGetTuple(tuplePattern: any[], del: boolean): any[] | null {
-
+	private async tryGetTuple(tuplePattern: any[], del: boolean): Promise<any[] | null> {
+		const method = del ? "DELETE" : "GET";
+		const path = del ? "rdp" : "inp";
+		const response = await this.sendJsonRequest(method, path, tuplePattern);
+		return await response.json();
 	}
 
-	public out(tuple: any[]): void {
-		return this.sendJsonRequest("POST", "out", tuple);
+	public async out(tuple: any[]): Promise<undefined> {
+		await this.sendJsonRequest("POST", "out", tuple);
 	}
 
-	public in(tuplePattern: any[]): any[] {
+	public in(tuplePattern: any[]): Promise<any[]> {
 		return this.waitTuple(tuplePattern, true);
 	}
 
-	public rd(tuplePattern: any[]): any[] {
+	public rd(tuplePattern: any[]): Promise<any[]> {
 		return this.waitTuple(tuplePattern, false);
 	}
 
-	public inp(tuplePattern: any[]): any[] | null {
+	public inp(tuplePattern: any[]): Promise<any[] | null> {
 		return this.tryGetTuple(tuplePattern, true);
 	}
 
-	public rdp(tuplePattern: any[]): any[] | null {
+	public rdp(tuplePattern: any[]): Promise<any[] | null> {
 		return this.tryGetTuple(tuplePattern, false);
 	}
 
-	public evalRegister(key: string, ironpythonCode: string): void {
-		return this.sendTextRequest("PUT", `eval/${key}`, ironpythonCode, "text/ironpython");
+	public async evalRegister(key: string, ironpythonCode: string): Promise<undefined> {
+		await this.sendTextRequest("PUT", `eval/${key}`, ironpythonCode, "text/ironpython");
 	}
 
-	public evalInvoke(key: string, parameter: any = null): void {
-		return this.sendJsonRequest("POST", `eval/${key}`, parameter);
+	public async evalInvoke(key: string, parameter: any = null): Promise<undefined> {
+		await this.sendJsonRequest("POST", `eval/${key}`, parameter);
 	}
 
-	public eval(ironpythonCode: string): void {
-		return this.sendTextRequest("POST", `eval`, ironpythonCode, "text/ironpython");
+	public async eval(ironpythonCode: string): Promise<undefined> {
+		await this.sendTextRequest("POST", `eval`, ironpythonCode, "text/ironpython");
+	}
+
+	public async isHealthy(): Promise<boolean> {
+		const url = this.healthUrl + "ping";
+
+		try {
+			const response = await fetch(url);
+			return response.ok;
+		} catch {
+			return false;
+		}
 	}
 }
