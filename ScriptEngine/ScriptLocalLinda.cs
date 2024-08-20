3c4c0353c22ed14ebd57d4cd37a65dc107fcc437
Đorđe Milanović <djordje200179@gmail.com>
2024-08-20 19:00:48 +0200

    Redudant conversion removed

--- ScriptEngine/ScriptLocalLinda.cs
+++ ScriptEngine/ScriptLocalLinda.cs
@@ -1,23 +1,14 @@
-﻿using IronPython.Runtime.Operations;
-
-namespace LindaSharp.ScriptEngine;
+﻿namespace LindaSharp.ScriptEngine;
 
 public class ScriptLocalLinda(IActionEvalLinda linda) {
-	private static object?[] ReformatTuple(object?[] tuple) {
-		return tuple.Select(elem => elem switch {
-			int intElem => intElem.ToBigInteger(),
-			_ => elem
-		}).ToArray();
-	}
-
 #pragma warning disable IDE1006 // Naming Styles
-	public void put(params object[] tuple) => linda.Put(ReformatTuple(tuple)!);
+	public void put(params object[] tuple) => linda.Put(tuple);
 
-	public object[] get(params object?[] pattern) => linda.Get(ReformatTuple(pattern)).Result;
-	public object[] query(params object[] pattern) => linda.Query(ReformatTuple(pattern)).Result;
+	public object[] get(params object?[] pattern) => linda.Get(pattern).Result;
+	public object[] query(params object[] pattern) => linda.Query(pattern).Result;
 
-	public object[]? try_get(params object?[] pattern) => linda.TryGet(ReformatTuple(pattern)).Result;
-	public object[]? try_query(params object?[] pattern) => linda.TryQuery(ReformatTuple(pattern)).Result;
+	public object[]? try_get(params object?[] pattern) => linda.TryGet(pattern).Result;
+	public object[]? try_query(params object?[] pattern) => linda.TryQuery(pattern).Result;
 
 	public void eval(Action<ILinda> function) => linda.Eval(function);
 #pragma warning restore IDE1006 // Naming Styles
