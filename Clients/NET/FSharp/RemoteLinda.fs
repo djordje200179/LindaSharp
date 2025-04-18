d99508ac632a0fcc03dae21adf1b0df7c55c0882
Đorđe Milanović <djordje200179@gmail.com>
2025-04-18 21:17:35 +0200

    Solution refactored

--- Clients/NET/FSharp/RemoteLinda.fs
+++ Clients/NET/FSharp/RemoteLinda.fs
@@ -1,26 +1,39 @@
-﻿module LindaSharp.Client.FSharp
+﻿module LindaSharp.Client.RemoteLinda
 
 open LindaSharp.Client
+open LindaSharp
 
-let inline put (linda: RemoteLinda) tuple = linda.Put tuple
+let inline put (linda: RemoteLinda) tuple = 
+    linda.Put tuple
+    |> Async.AwaitTask
 
-let inline get (linda: RemoteLinda) tuple = linda.Get tuple
+let inline get (linda: RemoteLinda) tuple = 
+    let tuple = Array.map Option.toObj<obj> tuple
+    
+    linda.Get tuple
+    |> Async.AwaitTask
 
-let inline query (linda: RemoteLinda) tuple = linda.Query tuple
+let inline query (linda: RemoteLinda) tuple = 
+    let tuple = Array.map Option.toObj<obj> tuple
+    
+    linda.Query tuple
+    |> Async.AwaitTask
 
 let inline tryGet (linda: RemoteLinda) tuple = async {
+    let tuple = Array.map Option.toObj<obj> tuple
+
     let! result = 
-        tuple
-        |> linda.TryGet 
+        linda.TryGet tuple
         |> Async.AwaitTask
 
     return ValueOption.ofObj result
 }
 
 let inline tryQuery (linda: RemoteLinda) tuple = async {
+    let tuple = Array.map Option.toObj<obj> tuple
+
     let! result = 
-        tuple
-        |> linda.TryQuery 
+        linda.TryQuery tuple
         |> Async.AwaitTask
 
     return ValueOption.ofObj result
@@ -31,6 +44,11 @@ let inline registerScript (linda: RemoteLinda) key script =
     |> linda.RegisterScript
     |> Async.AwaitTask
 
+let inline registerScriptFile (linda: RemoteLinda) key filePath =
+    (key, filePath)
+    |> linda.RegisterScriptFile
+    |> Async.AwaitTask
+
 let inline invokeScript (linda: RemoteLinda) script = 
     script
     |> linda.InvokeScript
@@ -39,6 +57,11 @@ let inline invokeScript (linda: RemoteLinda) script =
 let inline evalScript (linda: RemoteLinda) script = 
     script
     |> linda.EvalScript
+    |> Async.AwaitTask
+
+let inline evalScriptFile (linda: RemoteLinda) scriptFile = 
+    scriptFile
+    |> linda.EvalScriptFile
     |> Async.AwaitTask
 
 let inline getScriptExecutionStatus (linda: RemoteLinda) id =
---
+++ Clients/NET/Test/Program.fs
@@ -0,0 +1,50 @@
+﻿open LindaSharp.Client
+open System
+
+let rec waitForServer client =
+    printfn "Waiting for server..."
+
+    async {
+        let! isHealthy = RemoteLinda.isHealthy client
+
+        if not isHealthy then
+            do! Async.Sleep 1000
+            return! waitForServer client
+    }
+
+let client = new RemoteLinda("http://localhost:5001")
+    
+async {
+    do! waitForServer client
+
+    do! RemoteLinda.put client [| "a"; 0 |]
+    do! RemoteLinda.put client [| "b"; 1 |]
+    do! RemoteLinda.put client [| "ind"; 1 |]
+
+    do! RemoteLinda.registerScriptFile client "fib-calc" "FibonachiCalculation.py"
+
+    let! taskIds = 
+        [| 0 .. 7 |]
+        |> Array.map (fun _ -> RemoteLinda.invokeScript client "fib-calc")
+        |> Async.Parallel
+
+    for i in 2 .. 70 do
+        let! resultTuple = RemoteLinda.get client [| Some "fib"; Some i; None |]
+            
+        resultTuple[2]
+        :?> float
+        |> int64
+        |> printfn "Fib[%d] = %d" i
+
+    do! RemoteLinda.put client [| "done" |]
+
+    printfn "Waiting for a second..."
+    do! Async.Sleep 1000
+
+    for id in taskIds do
+        let! status = RemoteLinda.getScriptExecutionStatus client id
+        printfn "%d: %A" id status
+}
+|> Async.RunSynchronously
+
+Console.ReadLine() |> ignore
